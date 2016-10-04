d30898781a5289162e7ea363048edf8651e51084
mathieuvalois <mathieu.valois.chenier@gmail.com>
2016-10-04 03:06:15 -0400

    2nd change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,8 +79,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel.com/docs">Ta mere</a>
+                    <a href="https://laracasts.com">Ton Pere</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Post route for web forms
+Route::post('user/edit', function () {
+    return view('hello');
+});
